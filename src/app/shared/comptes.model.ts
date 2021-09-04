893deb08608038f8866bcff1465c1c97096c211e
abdallah trabelsi <abdallahtrabelsi4@gmail.com>
2021-09-04 18:08:12 +0100

    first commit

---
+++ src/app/shared/comptes.model.ts
@@ -0,0 +1,10 @@
+export class Compte {
+    CompteId: number=0;
+    CO: string='';
+    Lib: string='';
+    DB: string='';
+    CR: string='';
+    TypeCompte: string='';
+    OuvertDB :string='';
+    OuvertCR :string='';
+}
